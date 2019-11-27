bf3f5cd292fce49d81e795874225a100e6f27eb5
cen <autotos@163.com>
2019-11-28 07:52:09 +0800

    my php example

---
+++ php/base/datatype.php
@@ -0,0 +1,9 @@
+<?php 
+    $x = "Hello world";
+    echo $x;
+    echo "<br>";
+    $x = 'Hello world!';
+    echo $x;
+    $x = 'xxx';
+    echo $x;
+?>
---
+++ php/index.php
@@ -0,0 +1,6 @@
+<?php
+    $x = 1;
+    echo "Hello World!";
+    phpinfo();
+    echo $x;
+?>
